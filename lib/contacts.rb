f6ba8cf9aafb7c505c402be2e202b83ef47d9f04
Zachary Lehmann <zlehmann@gmail.com>
2018-09-21 16:45:59 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,8 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
   end
 end
