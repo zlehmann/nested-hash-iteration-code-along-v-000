dc63535939445f3b429c491d652bfd6700c281f7
Zachary Lehmann <zlehmann@gmail.com>
2018-09-21 16:46:53 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
     end
   end
 end
